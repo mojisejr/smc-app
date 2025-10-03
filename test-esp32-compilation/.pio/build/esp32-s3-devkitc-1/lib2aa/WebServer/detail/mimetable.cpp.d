3cc4c84948887c49264c102a8d53dad510fa5627
mojisejr <nonthasak.l@gmail.com>
2025-10-03 22:55:51 +0700

    feat: Refactor ESP32 template for built-in RS485 transceiver (Iter 13)
    - Implemented DE pin control for half-duplex RS485 communication
    - Updated pin configuration: DE_PIN 18, RX_PIN 16, TX_PIN 17
    - Added medical device compliance comments and safety controls
    - Created comprehensive RS485 connection documentation
    - Successfully tested compilation and validated all dependencies
    - Connection mapping: ESP32 A+ -> CU12 A, B- -> B, GND -> GND
    - CU12 Y/Z pins unused (full-duplex not required)
    - Added test compilation environment for validation

---
+++ test-esp32-compilation/.pio/build/esp32-s3-devkitc-1/lib2aa/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32-s3-devkitc-1/lib2aa/WebServer/detail/mimetable.cpp.o: \
+ C:/Users/Asus/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ C:/Users/Asus/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ C:/Users/Asus/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
